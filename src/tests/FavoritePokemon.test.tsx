17a2ba22a306509d0d19bdf97a2230596816b180
Samantha Siqueira <samantharsiqueira@gmail.com>
2024-02-19 18:35:47 +0000

    third one

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,30 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import { PokemonType } from '../types';
+import { FavoritePokemon } from '../pages';
+
+describe('Test Suite - Testes do About', () => {
+  test('No favorite pokemon found', () => {
+    // Acessar
+    renderWithRouter(<App />, { route: '/favorites' });
+    const notFoundEl = screen.getByText(/no favorite pokémon found/i);
+    // Agir
+    // Aferir
+    expect(notFoundEl).toBeInTheDocument();
+  });
+
+  test('rendered only favorite pokemons', () => {
+    const pokemonMock: PokemonType[] = [
+      { id: 1, name: 'Pikachu', type: 'Electric', image: 'pikachu.png', moreInfo: '', summary: '', averageWeight: { value: '', measurementUnit: '' }, foundAt: [] },
+      { id: 2, name: 'Alakazan', type: 'Psychic', image: 'alakazan.png', moreInfo: '', summary: '', averageWeight: { value: '', measurementUnit: '' }, foundAt: [] },
+    ];
+    // Acessar
+    renderWithRouter(<FavoritePokemon pokemonList={ pokemonMock } />);
+
+    // Agir
+    // Aferir
+    expect(screen.getByText('Pikachu')).toBeInTheDocument();
+    expect(screen.getByText('Alakazan')).toBeInTheDocument();
+  });
+});
