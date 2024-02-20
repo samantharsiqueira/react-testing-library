af800200f1242638dda6ad05d89a753bdba8d1cc
Samantha Siqueira <samantharsiqueira@gmail.com>
2024-02-20 18:58:45 +0000

    fifth one

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -1,7 +1,9 @@
-import { screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { vi } from 'vitest';
 import { Pokedex } from '../pages';
 import renderWithRouter from '../renderWithRouter';
+import { PokemonButtonsPanel } from '../components';
 
 // Mock da lista de pokemons
 const pokemons = [
@@ -79,6 +81,7 @@ test('filter buttons for each pokemon type', async () => {
   expect(screen.getByRole('button', { name: 'Electric' })).toBeInTheDocument();
   expect(screen.getByRole('button', { name: 'Fire' })).toBeInTheDocument();
   expect(screen.getByRole('button', { name: 'Psychic' })).toBeInTheDocument();
+
   // Os botões devem ser capturados pelo data-testid=pokemon-type-button.
   expect(screen.getAllByTestId('pokemon-type-button')).toHaveLength(3);
 });
@@ -88,10 +91,38 @@ test('all button reset the filter', async () => {
 // Acessar
   renderWithRouter(<Pokedex pokemonList={ pokemons } favoritePokemonIdsObj={ {} } />);
 
-  userEvent.click(await screen.findByRole('button', { name: 'All' }));
+  await userEvent.click(screen.getByRole('button', { name: 'All' }));
 
   // Verificar se todos os Pokémon estão sendo exibidos
   pokemons.forEach(async (pokemon) => {
     expect(await screen.findAllByText(pokemon.name)).toHaveLength(1);
   });
+});
+
+test('Fire button shows only Fire-type Pokémon', async () => {
+  // Acessar
+  renderWithRouter(<Pokedex pokemonList={ pokemons } favoritePokemonIdsObj={ {} } />);
+
+  // Agir
+  await userEvent.click(screen.getByRole('button', { name: 'Fire' }));
+
+  // Verificar se apenas Pokémon do tipo "Fire" estão sendo exibidos
+  pokemons.forEach(async (pokemon) => {
+    if (pokemon.type === 'Fire') {
+      expect(await screen.findAllByText(pokemon.name)).toHaveLength(1);
+    } else {
+      // Se não for do tipo "Fire", não deve ser exibido
+      expect(screen.queryByText(pokemon.name)).toBeNull();
+    }
+  });
+});
+
+test('verify if when clicked the "all" button calls filterPokemon function', () => {
+  const filterPokemon = vi.fn();
+
+  render(<PokemonButtonsPanel filterPokemon={ filterPokemon } pokemonTypes={ [] } />);
+
+  const allButton = screen.getByRole('button', { name: /all/i });
+  fireEvent.click(allButton);
+  expect(filterPokemon).toHaveBeenCalledWith('all');
 });
