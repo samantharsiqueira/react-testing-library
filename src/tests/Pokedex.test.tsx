47a9a975d540f856277cb5c0133a01e56f1cffc7
Samantha Siqueira <samantharsiqueira@gmail.com>
2024-02-20 17:18:53 +0000

    updating

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -36,17 +36,22 @@ const pokemons = [
     summary: '',
   },
 ];
-
+// Teste se a página contém um heading h2 com o texto Encountered Pokémon.
 test('heading Ecountered Pokemon', () => {
+  // Acessar
   renderWithRouter(<Pokedex pokemonList={ pokemons } favoritePokemonIdsObj={ {} } />);
   const headingEl = screen.getByRole('heading', { name: /encountered pokémon/i });
+  // Aferir
   expect(headingEl).toBeInTheDocument();
 });
 
+// Teste se é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado:
 test('Next Pokémon on the list is displayed when clicking the button', async () => {
+  // Acessar
   renderWithRouter(<Pokedex pokemonList={ pokemons } favoritePokemonIdsObj={ {} } />);
-
+  // Agir
   await userEvent.click(screen.getByRole('button', { name: /próximo pokémon/i }));
+  // Aferir
   expect(screen.getByText(pokemons[1].name)).toBeInTheDocument();
 
   await userEvent.click(screen.getByRole('button', { name: /próximo pokémon/i }));
@@ -56,7 +61,37 @@ test('Next Pokémon on the list is displayed when clicking the button', async ()
   expect(screen.getByText(pokemons[0].name)).toBeInTheDocument();
 });
 
-test('filter buttons', () => {});
+// Teste se é mostrado apenas um Pokémon por vez.
+test('Only one pokemon is showed at the time', () => {
+  // Acessar
+  renderWithRouter(<Pokedex pokemonList={ pokemons } favoritePokemonIdsObj={ {} } />);
+  // Aferir
+  expect(screen.getAllByTestId('pokemon-name')).toHaveLength(1);
+});
 
-test('reset filter button', async () => {
+// Teste se a Pokédex tem os botões de filtro:
+test('filter buttons for each pokemon type', async () => {
+  // Acessar
+  renderWithRouter(<Pokedex pokemonList={ pokemons } favoritePokemonIdsObj={ {} } />);
+
+  // Aferir
+  // Deve existir um botão de filtragem para cada tipo de Pokémon, sem repetição.
+  expect(screen.getByRole('button', { name: 'Electric' })).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: 'Fire' })).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: 'Psychic' })).toBeInTheDocument();
+  // Os botões devem ser capturados pelo data-testid=pokemon-type-button.
+  expect(screen.getAllByTestId('pokemon-type-button')).toHaveLength(3);
+});
+
+// A Pokedéx deverá mostrar os Pokémon normalmente (sem filtros) quando o botão All for clicado.
+test('all button reset the filter', async () => {
+// Acessar
+  renderWithRouter(<Pokedex pokemonList={ pokemons } favoritePokemonIdsObj={ {} } />);
+
+  userEvent.click(await screen.findByRole('button', { name: 'All' }));
+
+  // Verificar se todos os Pokémon estão sendo exibidos
+  pokemons.forEach(async (pokemon) => {
+    expect(await screen.findAllByText(pokemon.name)).toHaveLength(1);
+  });
 });
