31224b4000a1fb46ea8197b1e54f052d43e8cf10
Samantha Siqueira <samantharsiqueira@gmail.com>
2024-02-21 14:47:37 +0000

    sixth one

--- src/tests/Pokemon.test.tsx
+++ src/tests/Pokemon.test.tsx
@@ -1,1 +1,70 @@
-test('', () => {});
+import { screen, waitFor } from '@testing-library/react';
+
+import userEvent from '@testing-library/user-event';
+import { Pokemon } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+// Mock do Pokemone
+const PikachuSprite = 'Pikachu sprite';
+const mockPokemon = {
+  id: 1,
+  name: 'Pikachu',
+  type: 'Electric',
+  image: 'pikachu.png',
+  averageWeight: { value: '6', measurementUnit: 'kg' },
+  foundAt: [],
+  moreInfo: '',
+  summary: '',
+};
+
+// Verifica se o componente renderiza corretamente com as informações do Pokémon
+test('renders Pokemon card with correct information', () => {
+  renderWithRouter(<Pokemon
+    pokemon={ mockPokemon }
+    showDetailsLink
+    isFavorite={ false }
+  />);
+
+  expect(screen.getByTestId('pokemon-name')).toHaveTextContent('Pikachu');
+  expect(screen.getByTestId('pokemon-type')).toHaveTextContent('Electric');
+  expect(screen.getByTestId('pokemon-weight')).toHaveTextContent('Average weight: 6 kg');
+  expect(screen.getByAltText(PikachuSprite)).toBeInTheDocument();
+  expect(screen.getByRole('link')).toHaveAttribute('href', '/pokemon/1');
+});
+
+test('renders Pokemon card with correct information', async () => {
+  renderWithRouter(<Pokemon
+    pokemon={ mockPokemon }
+    showDetailsLink
+    isFavorite={ false }
+  />);
+
+  userEvent.click(screen.getByText('More details'));
+
+  // Aguarda a próxima renderização para verificar se a URL foi alterada
+  await waitFor(() => {
+    expect(window.location.pathname).toBe(`/pokemon/${mockPokemon.id}`);
+  });
+});
+
+test('favorite icon appears', () => {
+  renderWithRouter(<Pokemon
+    pokemon={ mockPokemon }
+    showDetailsLink
+    isFavorite
+  />);
+
+  expect(screen.getByAltText('Pikachu is marked as favorite')).toBeInTheDocument();
+  expect(screen.getByAltText('Pikachu is marked as favorite')).toHaveAttribute('src', '/star-icon.png');
+});
+
+test('renders Pokemon image', async () => {
+  renderWithRouter(<Pokemon
+    pokemon={ mockPokemon }
+    showDetailsLink
+    isFavorite={ false }
+  />);
+
+  expect(screen.getByAltText(PikachuSprite)).toBeInTheDocument();
+  expect(screen.getByAltText(PikachuSprite)).toHaveAttribute('src', 'pikachu.png');
+});
--- src/tests/PokemonDetails.test.tsx
+++ src/tests/PokemonDetails.test.tsx
@@ -1,1 +1,24 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+
+// A página deve conter um texto <name> Details, em que <name> é o nome do Pokémon.
+test('text <name> Pokemon Details', () => {});
+
+// Não deve existir o link de navegação para os detalhes do Pokémon selecionado.
+test('no Nav links to pokemons details', () => {});
+
+// A seção de detalhes deve conter um heading h2 com o texto Summary.
+test('heading h2 Summary text', () => {
+  // Acessar
+  expect(screen.getByRole('heading', { name: /summary/i })).toBeInTheDocument();
+});
+
+// A seção de detalhes deve conter um parágrafo com o resumo do Pokémon específico sendo visualizado.
+test('Pokemon brief in a p tag', () => {});
+
+// Teste se existe na página uma seção com os mapas contendo as localizações do Pokémon:
+test('location section in page', () => {});
+
+// A página deve exibir um checkbox que permite favoritar o Pokémon.
+// Cliques alternados no checkbox devem adicionar e remover, respectivamente, o Pokémon da lista de favoritos.
+// O label do checkbox deve conter o texto Pokémon favoritado?.
+test('checkbox tests', () => {});
