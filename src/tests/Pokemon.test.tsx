7efe327cbf4fedffc83ff7564d833ddab5dc44b8
Samantha Siqueira <samantharsiqueira@gmail.com>
2024-02-21 15:50:05 +0000

    seventh tentando

--- src/tests/Pokemon.test.tsx
+++ src/tests/Pokemon.test.tsx
@@ -1,5 +1,4 @@
 import { screen, waitFor } from '@testing-library/react';
-
 import userEvent from '@testing-library/user-event';
 import { Pokemon } from '../components';
 import renderWithRouter from '../renderWithRouter';
--- src/tests/PokemonDetails.test.tsx
+++ src/tests/PokemonDetails.test.tsx
@@ -1,24 +1,74 @@
 import { screen } from '@testing-library/react';
+import { vi } from 'vitest';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import pokemonList from '../data';
+import * as mocks from '../services/pokedexService';
 
-// A página deve conter um texto <name> Details, em que <name> é o nome do Pokémon.
-test('text <name> Pokemon Details', () => {});
+// Desestruturação para obter o array 'foundAt' do primeiro Pokémon na lista
+const { foundAt: PokemonLocations } = pokemonList[0];
 
-// Não deve existir o link de navegação para os detalhes do Pokémon selecionado.
-test('no Nav links to pokemons details', () => {});
+test('Pokemon detailed informations are rendered', async () => {
+  // Renderiza o componente 'App' envolto em um objeto 'user' retornado por 'renderWithRouter'
+  const { user: userActions } = renderWithRouter(<App />);
 
-// A seção de detalhes deve conter um heading h2 com o texto Summary.
-test('heading h2 Summary text', () => {
-  // Acessar
+  // Simula o clique em um link com o texto "More details"
+  await userActions.click(screen.getByRole('link', { name: /more details/i }));
+
+  // Verifica o que aparece no mais detalhes
+  expect(screen.getByText(/pikachu details/i)).toBeInTheDocument();
+  expect(screen.queryByRole('link', { name: /more details/i })).not.toBeInTheDocument();
   expect(screen.getByRole('heading', { name: /summary/i })).toBeInTheDocument();
+
+  // Verifica se o texto contendo "hard berries with electricity" está presente na tela
+  expect(screen.getByText(/hard berries with electricity /i));
 });
 
-// A seção de detalhes deve conter um parágrafo com o resumo do Pokémon específico sendo visualizado.
-test('Pokemon brief in a p tag', () => {});
+test('There is a location and maps section', () => {
+  renderWithRouter(<App />, { route: '/pokemon/1' });
 
-// Teste se existe na página uma seção com os mapas contendo as localizações do Pokémon:
-test('location section in page', () => {});
+  expect(screen.getByRole('heading', { name: /game locations of pikachu/i }))
+    .toBeInTheDocument();
 
-// A página deve exibir um checkbox que permite favoritar o Pokémon.
-// Cliques alternados no checkbox devem adicionar e remover, respectivamente, o Pokémon da lista de favoritos.
-// O label do checkbox deve conter o texto Pokémon favoritado?.
-test('checkbox tests', () => {});
+  // Itera sobre a lista de locais onde o Pikachu pode ser encontrado e verifica se cada um está presente na tela
+  if (pokemonList[0]?.foundAt) {
+    pokemonList[0].foundAt.forEach((value) => {
+      expect(screen.getByText(value.location)).toBeInTheDocument();
+    });
+  }
+  // Testa as imagens com o atributo 'alt' contendo "pikachu location"
+  const imgEl = screen.getAllByAltText(/pikachu location/i);
+  expect(imgEl).toHaveLength(2);
+  if (PokemonLocations && PokemonLocations[0] && PokemonLocations[1]) {
+    expect(imgEl[0]).toHaveAttribute('src', PokemonLocations[0].map);
+    expect(imgEl[1]).toHaveAttribute('src', PokemonLocations[1].map);
+  }
+});
+
+test('Favorite button is working properly', async () => {
+  const { user: userActions } = renderWithRouter(<App />, { route: '/pokemon/1' });
+
+  // Espiona a função 'getFavoritePokemonList' do módulo 'mocks'
+  const mocked = vi.spyOn(mocks, 'getFavoritePokemonList');
+
+  // Obtém o checkbox com o rótulo "Pokémon favoritado?"
+  const checkbox = screen.getByRole('checkbox', { name: 'Pokémon favoritado?' });
+
+  // Verifica se o checkbox está presente na tela
+  expect(checkbox).toBeInTheDocument();
+
+  // Simula o clique no checkbox
+  await userActions.click(checkbox);
+
+  // Verifica se a função 'getFavoritePokemonList' foi chamada
+  expect(mocked).toHaveBeenCalled();
+
+  // Verifica se o Pokémon foi marcado como favorito na lista retornada pela função espionada
+  expect(mocked.mock.results[0].value[1]).toBeTruthy();
+
+  // Simula o clique novamente no checkbox para desmarcar o Pokémon como favorito
+  await userActions.click(checkbox);
+
+  // Verifica se o Pokémon foi desmarcado como favorito na lista retornada pela função espionada
+  expect(mocked.mock.results[3].value[1]).toBeFalsy();
+});
